87b199c2193352a08907bd70a24a6ce7685d1139
SamiraMicaela <micaelabava@hotmail.es>
2023-03-23 16:18:07 -0300

    trabajos

--- diapositiva/descuento.ts
+++ diapositiva/descuento.ts
@@ -3,5 +3,20 @@ let precioFinal : number =0;
 let precioProductoUno: number =200;
 let precioProductoDos: number = 350;
 
-let cantidadDelProductoUno : number = rls.questionInt ("Cantidad de producto 1 que compro el usuario")
-let cantidadDelProductoUdos : number = rls.questionInt ("Cantidad de producto 2 que compro el usuario")
+let cantidadDelProductoUno : number = rls.questionInt ("Cantidad de producto 1 que compro el usuario");
+let cantidadDelProductoDos : number = rls.questionInt ("Cantidad de producto 2 que compro el usuario");
+
+let subTotal : number = 0;
+let valorDescuento : number = 0;
+subTotal = (cantidadDelProductoUno * precioProductoUno) + (cantidadDelProductoDos * precioProductoDos);
+
+console.log("cantidad del producto 1:", cantidadDelProductoUno, " \ncantidad de producto 2:", cantidadDelProductoDos,"\nsubtotal:" , subTotal );
+
+if (subTotal >1000){
+    valorDescuento = subTotal * 0.1;
+    precioFinal = (subTotal - valorDescuento);
+    console.log("uds ahorra: $", valorDescuento, "por haber comprado mas de $1000 \nel precio final es de:", precioFinal);
+} else{
+    precioFinal = subTotal;
+    console.log ("gracias por confiar en nosotros. \nel precio final es de :$", precioFinal);
+}
---
+++ diapositiva/log.ts
@@ -0,0 +1,11 @@
+import * as rls from "readline-sync"; 
+let Samira : boolean = true;
+let claveSamira : boolean= true;
+let user : string = rls.question("ingrese el nombre de usuario: ");
+let clave : string= rls.question("ingrese la clave: ");
+
+if (user=="Samira" && clave=="claveSamira"){
+    console.log("ingresaste los datos correctos");
+} else {
+    console.log("usted no a ingresado los datos correctos");
+}
