b888d6cd5f4e5d2d2a59c869dcf96737a975dbfa
SamiraMicaela <micaelabava@hotmail.es>
2023-03-23 14:49:40 -0300

    trabajos

---
+++ diapositiva/descuento.ts
@@ -0,0 +1,7 @@
+import * as rls from "readline-sync"
+let precioFinal : number =0;
+let precioProductoUno: number =200;
+let precioProductoDos: number = 350;
+
+let cantidadDelProductoUno : number = rls.questionInt ("Cantidad de producto 1 que compro el usuario")
+let cantidadDelProductoUdos : number = rls.questionInt ("Cantidad de producto 2 que compro el usuario")
