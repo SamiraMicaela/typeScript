8c765dbd0123a3aad9c1d64b3ee810b3b4b390c4
SamiraMicaela <micaelabava@hotmail.es>
2023-03-24 17:06:58 -0300

    ejercicios

---
+++ mayorDeTres.ts
@@ -0,0 +1,15 @@
+import * as rls from "readline-sync"
+const n1 : number = rls.questionFloat("ingrese un numero: ");
+const n2 : number = rls.questionFloat("ingrese un numero: ");
+const n3 : number = rls.questionFloat("ingrese un numero: ");
+if (n1>=n2 && n1>=n3){
+    console.log("el numero mayor es:", n1);
+    } else if (n2>=n1 && n2>=n3){
+        console.log("el numero mayor es:", n2);
+    } else if (n3>=n1 && n3>=n2){
+        console.log("el numero mayor es:", n3);
+    } else if (n1===n2 && n1===n3){
+        console.log("el numero mayor es:", n1);
+    } 
+    
+ 
